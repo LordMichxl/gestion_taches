834cb74d1d78db888bc07cd2659ef00f600e26d6
Gaus <capidelamordicus@gmail.com>
2026-02-02 23:15:05 +0000

    Initial commit des vues login gestion_taches

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,12 @@ Route::get('/tasks/create', function () {
 
 
 // routes l'authentification
+use App\Http\Controllers\RegisterController;
+
+// Inscription
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
 Route::middleware('auth')->group(function () {
     // Modification du profil utilisateur
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
