99b0a250a4e79b0b33a8fd4ac4b43f0b9ce90939
Michel Basse <segou.michel@gmail.com>
2026-02-03 19:39:56 +0000

    "Corrections"

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
